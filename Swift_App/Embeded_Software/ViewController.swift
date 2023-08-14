04f5af5b59e9a3f56883951ba8ce9ecd70dfa77e
Taeyoon Lee <132141316+taeyoonL@users.noreply.github.com>
2023-08-14 20:29:59 +0900

    Update ViewController.swift

--- Swift_App/Embeded_Software/ViewController.swift
+++ Swift_App/Embeded_Software/ViewController.swift
@@ -19,14 +19,17 @@ class ViewController: UIViewController, MFMessageComposeViewControllerDelegate {
     
     @IBOutlet var label_5: UILabel!
     
+    @IBOutlet var label_6: UILabel!
+    
+    @IBOutlet var label_7: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         label_1.text = "Black Wrist을(를) 통해,"
         label_2.text = "LSD 마약이 감지되었습니다."
-        //label_3.text = "자동으로 사법 기관에 연락이 진행되며,"
-        //label_4.text = "이를 무시하고 싶으신 경우, 30초 안에 Bar를 밀어주세요."
+        label_3.text = "자동으로 사법 기관에 연락이 진행되며,"
+        label_4.text = "이를 무시하고 싶으신 경우, 30초 안에 Bar를 밀어주세요."
         Timer.scheduledTimer(timeInterval: 30.0, target: self, selector: #selector(ViewController.send_auto_message), userInfo: nil, repeats: false)
     }
 
@@ -44,7 +47,7 @@ class ViewController: UIViewController, MFMessageComposeViewControllerDelegate {
     }
     
     @objc func send_auto_message(){
-        let phone_number = "112"
+        let phone_number = "01082129547"
         let message = "현재 마약에 노출되었습니다. 빨리 와주세요. 현재 저의 위치는 ~~~ 입니다."
         sendTextMessage(to: phone_number, message: message)
     }
@@ -76,4 +79,3 @@ class ViewController: UIViewController, MFMessageComposeViewControllerDelegate {
         controller.dismiss(animated: true, completion: nil)
     }
 }
-
